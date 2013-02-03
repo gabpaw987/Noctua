fba0aa3722744f2ad14d5ee70e4d2a6214b674e7
Josef Sochovsky <josef.nikolaus@sochovsky.at>
2013-02-03 18:14:21 +0100

    Added sma+momentum-trippleCrossed101520.dll

--- trunk/Algorithms/Algorithms/SMA.fs
+++ trunk/Algorithms/Algorithms/SMA.fs
@@ -1,14 +1,7 @@
 ﻿
 // Weitere Informationen zu F# unter "http://fsharp.net".
 namespace Algorithm
-    module DecisionCalculator=
-        open System.IO
-        let readLines (filePath:string) = seq {
-            use sr = new StreamReader (filePath)
-            while not sr.EndOfStream do
-                yield sr.ReadLine ()
-        }
-        
+    module DecisionCalculator=        
         (* This function calculates the simple moving average for a list of lists of decimal and returns the calculated values in a list of decimals*)
         let sma(n:int, liste2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>)=
             let mutable sma = [];
@@ -37,7 +30,49 @@ namespace Algorithm
                 ergebnis1 <- List.append ergebnis1 [decimal c1.Series.["indicator"].Points.[i].YValues.[0]]
             ergebnis1
         
-        (* smaklein gegen smagross *)
+        (*
+            This function calculates the momentum over the give time period
+        *)
+        let momentum(period:int, prices:System.Collections.Generic.List<System.Tuple<System.DateTime,decimal, decimal, decimal, decimal>>)=
+            let mutable result = []
+            for i = period to prices.Count - 1 do
+                result <- List.append result [prices.[i].Item5 - prices.[i - period].Item5]
+            result
+        
+        (*
+            This function calls the momentum function and interprets its results
+        *)
+        let momentumInterpreter(period:int, prices:System.Collections.Generic.List<System.Tuple<System.DateTime,decimal, decimal, decimal, decimal>>)=
+            let moments = momentum(period, prices)
+            // mom pos und steigend +3
+            let mutable m1 = 0
+            // mom pos und fallend +1
+            let mutable m2 = 0
+            // mom neg und fallend -3
+            let mutable m3 = 0
+            // mom neg und steigend -1
+            let mutable m4 = 0
+            for i = 1 to moments.Length - 1 do 
+                if(moments.[i]>0m&&moments.[i-1]<moments.[i]) then
+                    m1 <- m1 + 1
+                if(moments.[i]>0m&&moments.[i-1]>moments.[i]) then
+                    m2 <- m2 + 1
+                if(moments.[i]<0m&&moments.[i-1]>moments.[i]) then
+                    m3 <- m3 + 1
+                if(moments.[i]<0m&&moments.[i-1]<moments.[i]) then
+                    m4 <- m4 + 1
+            if ((m1>m2&&m1>m3&&m1>m4)||m3>m1&&m3>m2&&m3>m4) then
+                3
+            else if ((m2>m1&&m2>m3&&m2>m4)||m4>m1&&m4>m2&&m4>m3) then
+                1
+            else
+                0
+
+        (*  two sma´s
+            param:
+                n1: smaller range of sma
+                n2: higher range of sma     
+        *)
         let signalgeber1(n1:int, n2:int,list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
             for i = 0 to n2-1 do
                 signals.Add(0)
@@ -53,7 +88,9 @@ namespace Algorithm
                     signals.Add(0)
             signals
 
-            (* sma gegen Preis*)
+        (* 
+            SMA vs. Price
+        *)
         let signalgeber4(n:int,list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
             for i = 0 to n-1 do
                 signals.Add(0)
@@ -66,7 +103,9 @@ namespace Algorithm
                 if list2D.[i+n-1].Item5 = ergebnis1.[i] then
                     signals.Add(0)
             signals
-        
+        (*
+            this signalgenerator adds a percentage fall to the calcuation system to cut loss
+        *)
         let signalgeber3(n1:int, n2:int,percentage:double, list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
             for i = 0 to n2-1 do
                 signals.Add(0)
@@ -93,40 +132,19 @@ namespace Algorithm
                     signals.Add(0)
             signals
 
-        (* Neu mit FinancialFormulars*)
-        let signalgeber2(n:int,list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
-            for i = 0 to n-2 do
-                signals.Add(0)
-            let s1 = new System.Windows.Forms.DataVisualization.Charting.Series("historicalData")
-            s1.ChartType <- System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick
-            let s2 = new System.Windows.Forms.DataVisualization.Charting.Series("incdicator")
-            for i = 0 to list2D.Count - 1 do
-                let mutable temp:int = s1.Points.AddXY(list2D.[i].Item1,double list2D.[i].Item3)
-                let mutable liste = [|double list2D.[i].Item4;double list2D.[i].Item2;double list2D.[i].Item5|]
-                s1.Points.[i].YValues <- Array.append s1.Points.[i].YValues liste
-                temp <- temp+1
-            let c1 = new System.Windows.Forms.DataVisualization.Charting.Chart()
-            c1.Series.Add(s1)
-            c1.Series.Add(s2)
-            c1.DataManipulator.FinancialFormula(System.Windows.Forms.DataVisualization.Charting.FinancialFormula.MovingAverage,"90","historicalData:Y3","indicator")
-            let mutable ergebnis1 = []
-            for i = 0 to c1.Series.["indicator"].Points.Count - 1 do 
-                ergebnis1 <- List.append ergebnis1 [decimal c1.Series.["indicator"].Points.[i].YValues.[0]]
-            for i = 0 to ergebnis1.Length-1 do
-                if list2D.[i+n-1].Item5 < ergebnis1.[i] then
-                    signals.Add(-1)
-                if list2D.[i+n-1].Item5 > ergebnis1.[i] then
-                    signals.Add(1)
-                if list2D.[i+n-1].Item5 = ergebnis1.[i] then
-                    signals.Add(0)
-            signals
-        
-        (* smaklein gegen smagross *)
+        (*  two sma´s
+            param:
+                n1: smaller range of sma
+                n2: higher range of sma     
+        *)
         let signalgeber(n1:int, n2:int,list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
+            // add zeros
             for i = 0 to n2-2 do
                 signals.Add(0)
+            // calculate the two sma´s
             let ergebnis1 = sma2(n1,list2D)
             let ergebnis2 = sma2(n2,list2D)
+            // the number of indices the smaller is ahead
             let mover = n2 - n1
             for i = 0 to ergebnis2.Length-1 do
                 if ergebnis1.[i+mover] > ergebnis2.[i] then
@@ -148,14 +166,18 @@ namespace Algorithm
             let longs = sma2(n3,list2D)
             for i = 0 to longs.Length - 1 do
                 if shorts.[i + shortmover] < middle.[i + middlemover] && middle.[i + middlemover] < longs.[i] then
-                    signals.Add(-1)
+                    signals.Add(-1*momentumInterpreter(5,list2D.GetRange(i+n3-10,10)))
                 else if shorts.[i + shortmover] > middle.[i + middlemover] && middle.[i + middlemover] > longs.[i] then
-                    signals.Add(1)
+                    signals.Add(1*momentumInterpreter(5,list2D.GetRange(i+n3-10,10)))
                 else
-                    signals.Add(signals.[signals.Count-1])
+                    // add the last again
+                    // signals.Add(signals.[signals.Count-1])
+                    // add zero
+                    signals.Add(0)
             signals.RemoveAt(signals.Count-1)
             signals
 
+        
 
         let startCalculation (list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>)= 
             tripleCrossed (10,15,20, list2D, signals)
