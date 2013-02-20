251c8fbc624c136c3ef967ab5b454a1652a4b7e5
Josef Sochovsky <josef.nikolaus@sochovsky.at>
2013-02-20 15:28:52 +0100

    Changed RSI.fs to work properly

--- trunk/Algorithms/Algorithms/RSI.fs
+++ trunk/Algorithms/Algorithms/RSI.fs
@@ -1,17 +1,15 @@
 ﻿
 namespace Algorithm
     module DecisionCalculator2=
-        let rsi(n:int, liste2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>) =
-            let mutable rsi = []
-            for i = liste2D.Count - n to liste2D.Count - 1 do
-                let mutable tempListe = liste2D.GetRange(i - n, n)
-                let mutable wins = []
-                let mutable losses = []
-                for j = 0 to tempListe.Count - 1 do 
-                    if tempListe.[j].Item2 < tempListe.[j].Item5 then 
-                        wins <- List.append wins [tempListe.[j].Item5-tempListe.[j].Item2]
-                    if tempListe.[j].Item5 < tempListe.[j].Item2 then 
-                        losses <- List.append losses [tempListe.[j].Item5-tempListe.[j].Item2]
-                rsi <- List.append rsi [100m*(List.sum(wins)/(decimal wins.Length))/((List.sum(wins)/(decimal wins.Length))-(List.sum(losses)/(decimal losses.Length)))]
-            rsi
+        let rsi(liste2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>) =
+            let mutable sumup = 0m
+            let mutable sumdown = 0m
+            for i = 1 to liste2D.Count - 1 do 
+                if(liste2D.[i].Item5>liste2D.[i-1].Item5)then
+                    sumup <- sumup + liste2D.[i].Item5
+                else if(liste2D.[i].Item5<liste2D.[i-1].Item5)then
+                    sumdown <- sumdown - liste2D.[i].Item5
+            //sumup/(decimal liste2D.Count)
+            //sumdown/(decimal liste2D.Count)
+            (sumup/(decimal liste2D.Count))/((sumup/(decimal liste2D.Count))+(sumdown/(decimal liste2D.Count)))
             
