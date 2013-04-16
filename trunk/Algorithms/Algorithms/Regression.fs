b6041d75c6f14bcb5483df2ec1250acccb591228
Josef Sochovsky <josef.nikolaus@sochovsky.at>
2013-04-16 17:57:06 +0200

    added Signalgeber for regression

--- trunk/Algorithms/Algorithms/Regression.fs
+++ trunk/Algorithms/Algorithms/Regression.fs
@@ -1,53 +1,48 @@
 ﻿namespace Algorithm
-    module DecisionCalculator9=
-        let regression(n:int, liste2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>)=
-            let s1 = new System.Windows.Forms.DataVisualization.Charting.Series("historicalData")
-            s1.ChartType <- System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick
-            let s2 = new System.Windows.Forms.DataVisualization.Charting.Series("incdicator")
-            for i = 0 to liste2D.Count - 1 do
-                let mutable temp:int = s1.Points.AddXY(liste2D.[i].Item1,double liste2D.[i].Item3)
-                let mutable liste = [|double liste2D.[i].Item4;double liste2D.[i].Item2;double liste2D.[i].Item5|]
-                s1.Points.[i].YValues <- Array.append s1.Points.[i].YValues liste
-                temp <- temp+1
-            let c1 = new System.Windows.Forms.DataVisualization.Charting.Chart()
-            c1.Series.Add(s1)
-            c1.Series.Add(s2)
-            c1.DataManipulator.FinancialFormula(System.Windows.Forms.DataVisualization.Charting.FinancialFormula.Forecasting,"linear ,"+(string n)+", true, true","historicalData:Y3","indicator")
-            let mutable ergebnis1 = []
-            for i = 0 to c1.Series.["indicator"].Points.Count - 1 do 
-                ergebnis1 <- List.append ergebnis1 [decimal c1.Series.["indicator"].Points.[i].YValues.[0]]
-            ergebnis1
-        
-        let regression2(liste2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>)=
+    module DecisionCalculator=
+        let regression(prices:decimal array)=
             let mutable xy = 0m
             let mutable xx = 0m
             let mutable x = 0m
             let mutable y = 0m
-            for i = 0 to liste2D.Count - 1 do 
+            for i = 0 to prices.Length - 1 do 
                 x <- x+decimal i
-                y <- y + liste2D.[i].Item5
-                xy <- xy + liste2D.[i].Item5 * decimal i
+                y <- y + prices.[i]
+                xy <- xy + prices.[i] * decimal i
                 xx <- xx + decimal i* decimal i
-            let b = (decimal liste2D.Count * xy - (x*y))/(decimal liste2D.Count * xx - x*x)
-            let a = (y - b*x)/decimal liste2D.Count
-            decimal liste2D.Count*b + a
+            let b = (decimal prices.Length * xy - (x*y))/(decimal prices.Length * xx - x*x)
+            let a = (y - b*x)/decimal prices.Length
+            //decimal liste2D.Count*b + a
+            b
 
         (*  *)
-        let signalgeber(n:int,list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
-            //for i = 0 to 2 do
-            //    signals.Add(0)
-            let mutable regressions = []
-            for i = 20 to list2D.Count - 1 do
-                regressions <- List.append regressions [(regression2((list2D.GetRange(i-20,20))))]
-            let regression = regression2(list2D)
-            let temp = regressions.[0]
-            for i = 0 to regressions.Length - 1 do
-                let temp = regressions.[i]
-                if regressions.[i]>0m then
+        let signalgeber(n:int,prices:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>) =
+            // skip already calculated signals
+            let skip = if signals.Count-n+1 > 0 then signals.Count-n+1 else 0
+            let cPrices = 
+                [ for i in prices -> i.Item5 ]
+                |> Seq.skip skip
+                |> Seq.toArray
+            
+            let longregressions = Array.append (Array.zeroCreate(20)) [|for i in 20 .. prices.Count - 1 do yield regression([|for j in i - 20 .. i do yield cPrices.[j]|])|]
+            let shortregressions = Array.append (Array.zeroCreate(5)) [|for i in 5 .. prices.Count - 1 do yield regression([|for j in i - 5 .. i do yield cPrices.[j]|])|]
+            for i in 0 .. prices.Count - 1 do 
+                if(longregressions.[i]>0.6m && shortregressions.[i]>0m) then 
+                    signals.Add(3)
+                else if(longregressions.[i]>0.4m && shortregressions.[i]>0m) then 
+                    signals.Add(2)
+                else if(longregressions.[i]>0.2m && shortregressions.[i]>0m) then 
                     signals.Add(1)
-                else if regressions.[i]<0m then
-                    signals.Add(-1)    
+                else if(longregressions.[i] > -0.6m && shortregressions.[i]<0m) then 
+                    signals.Add(-3)
+                else if(longregressions.[i] > -0.4m && shortregressions.[i]<0m) then 
+                    signals.Add(-2)
+                else if(longregressions.[i] > -0.2m && shortregressions.[i]<0m) then 
+                    signals.Add(-1)
+                else signals.Add(0)
+
             signals
 
         let startCalculation (list2D:System.Collections.Generic.List<System.Tuple<System.DateTime, decimal, decimal, decimal, decimal>>,signals:System.Collections.Generic.List<int>)= 
             signalgeber (20, list2D, signals)
+            
